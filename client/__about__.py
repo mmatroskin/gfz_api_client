c0e18460b2e4f0c1ed976f25880cbab21edc4b69
Maksim Tulin <maksim.tulin@x5.ru>
2025-01-15 19:47:58 +0300

    [dev] description added

---
+++ client/__about__.py
@@ -0,0 +1,8 @@
+import pathlib
+
+
+__version__ = pathlib.Path("VERSION").read_text().strip()
+
+__author__ = "Maksim Tulin"
+__copyright__ = "Copyright {}".format(__author__)
+__cake__ = "The \U0001f370 is a lie!"
